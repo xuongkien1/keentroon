1450af405ad343585c553900016c0e9eb444f29f
Rails Udemy Course <truon01@gmail.com>
2017-05-29 01:42:39 +0000

    Add admin user funtionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518221204) do
+ActiveRecord::Schema.define(version: 20170529002902) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -41,10 +41,11 @@ ActiveRecord::Schema.define(version: 20170518221204) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "user_id"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
